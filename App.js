915b46dd6453916b1a99983a684f46bfa3b47d67
ejjang2030 <ejjang2030@gmail.com>
2022-11-30 14:54:21 +0900

    react native counter 앱 만들기

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from "react";
+import { Button, SafeAreaView, Text, View, StyleSheet } from "react-native";
+import Box from "./components/Box";
+import Counter from "./components/Counter";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+
+  return (
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
+
+export default App;
